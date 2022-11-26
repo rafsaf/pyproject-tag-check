3541866eef2d21cb31b12cedd2f91cdd5b3a9946
rafsaf <rafal.safin12@gmail.com>
2022-11-26 02:46:23 +0100

    Create package code, define pre-commit hooks and publish CI

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# pyproject-tag-check
+## pyproject-tag-check
+
+I always forget to bump poetry version in pyproject.toml files. That's why I build this simple package. It is pre-commit hook which check that version in pyproject.toml is not used as a tag for given repo URL.
+
+## Usage:
+
+
+Put it in `.pre-commit.config.yaml` repos and argument must be URL to repo on GH (for example https://github.com/rafsaf/Tribal-Wars-Planer).
+
+```yml
+repos:
+  - repo: https://github.com/rafsaf/pyproject-tag-check
+    rev: "0.1.0"
+    hooks:
+      - id: pyproject-tag-check
+        args:
+          - https://github.com/rafsaf/Tribal-Wars-Planer
+
+```
---
+++ pyproject_tag_check/__init__.py
@@ -0,0 +1,33 @@
+import argparse
+import sys
+
+import requests
+import toml
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Verify version in pyproject.toml is not already used"
+    )
+    parser.add_argument(
+        "--repo_url",
+        help="Full URL to github repo that tag should be checked",
+        required=True,
+    )
+    parser.add_argument("pyproject_file", help="Path to pyproject.toml file")
+    args = parser.parse_args()
+
+    with open(args.pyproject_file, "r") as f:
+        pyproject_toml = toml.load(f)
+        version = pyproject_toml["tool"]["poetry"]["version"]
+
+    url = f"{args.repo_url}/releases/tag/{version}"
+    r = requests.get(url=url, stream=True)
+    # 404 means such a tag is not used, 200 otherwise
+    if not r.status_code == 404:
+        sys.exit(f"Tag {version} from {args.pyproject_file} already exists at {url}")
+    print("OK")
+
+
+if __name__ == "__main__":
+    main()
